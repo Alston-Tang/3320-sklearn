d77d798429c7de9f139f582112a8bb7a6e87022e
Alston Tang <tanghaomo@126.com>
2016-03-11 19:17:47 +0800

    finish code

--- simple_clf.py
+++ simple_clf.py
@@ -17,3 +17,24 @@ y[n_samples // 2:] = 1
 X_train, X_test, y_train, y_test = train_test_split(X, y,random_state=42)
 log_reg = linear_model.LogisticRegression()
 
+n_test_samples = X_test.shape[0]
+
+log_reg.fit(X_train, y_train)
+y_predict = log_reg.predict(X_test)
+
+# Todo: write to report
+# The predictions only have 0 and 1: no
+
+e_count = 0
+
+for i in range(0, n_test_samples):
+    if y_predict[i] != y_test[i]:
+        e_count += 1
+    if y_predict[i] == 0:
+        plt.scatter(X_test[i][0], X_test[i][1], c="B")
+    else:
+        plt.scatter(X_test[i][0], X_test[i][1], c="R")
+plt.show()
+# Todo: write to report
+print ("Number of wrong predictions is: {0}".format(e_count))
+
